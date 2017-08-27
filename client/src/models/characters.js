a7fe04bf4f92abbc92ed8e07e85d7fe399078700
James Cox <j-cox03@hotmail.com>
2017-08-27 13:57:03 +0100

    Made characters

--- client/src/models/characters.js
+++ client/src/models/characters.js
@@ -2,18 +2,87 @@
 const characters = [];
 
 const alex = {
-  name: "Alex",
-  gender: "Male",
+  name: "alex",
+  gender: "male",
   hair: {has: true, colour: "black"},
   nose: {size: "small"},
-  mustache: {has: true},
-  beard: {has: false},
+  mustache: {has: true, colour: "black"},
+  beard: {has: false, colour: "none"},
   glasses: {has: false},
   hat: {has: false, colour: "none"}
 };
+characters.push(alex);
 
-characters.push(
+const alfred = {
+  name: "alfred",
+  gender: "male",
+  hair: {has: true, colour: "ginger"},
+  nose: {size: "small"},
+  mustache: {has: true, colour: "ginger"},
+  beard: {has: false, colour: "none"},
+  glasses: {has: false},
+  hat: {has: false, colour: "none"}
+};
+characters.push(alfred);
 
-);
+const anita = {
+  name: "anita",
+  gender: "female",
+  hair: {has: true, colour: "blonde"},
+  nose: {size: "small"},
+  mustache: {has: false, colour: "none"},
+  beard: {has: false, colour: "none"},
+  glasses: {has: false},
+  hat: {has: false, colour: "none"}
+};
+characters.push(anita);
+
+const anne = {
+  name: "anne",
+  gender: "female",
+  hair: {has: true, colour: "black"},
+  nose: {size: "large"},
+  mustache: {has: false, colour: "none"},
+  beard: {has: false, colour: "none"},
+  glasses: {has: false},
+  hat: {has: false, colour: "none"}
+};
+characters.push(anne);
+
+const bernard = {
+  name: "bernard",
+  gender: "male",
+  hair: {has: true, colour: "brown"},
+  nose: {size: "large"},
+  mustache: {has: false, colour: "none"},
+  beard: {has: false, colour: "none"},
+  glasses: {has: false},
+  hat: {has: true, colour: "grey"}
+};
+characters.push(bernard);
+
+const bill = {
+  name: "bill",
+  gender: "male",
+  hair: {has: false, colour: "none"},
+  nose: {size: "small"},
+  mustache: {has: false, colour: "none"},
+  beard: {has: true, colour: "ginger"},
+  glasses: {has: false},
+  hat: {has: false, colour: "none"}
+};
+characters.push(bill);
+
+const charles = {
+  name: "charles",
+  gender: "male",
+  hair: {has: true, colour: "blonde"},
+  nose: {size: "small"},
+  mustache: {has: true, colour: "blonde"},
+  beard: {has: false, colour: "none"},
+  glasses: {has: false},
+  hat: {has: false, colour: "none"}
+};
+characters.push(charles);
 
 export default characters;
